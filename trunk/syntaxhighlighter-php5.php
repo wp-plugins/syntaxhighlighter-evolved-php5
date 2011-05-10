958d449f6121164e64300bc8552ee4a46a207f9e
sandrobilbeisi <sandrobilbeisi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-10 11:08:30 +0000

    Tagging the 1.4.1 release of the project.
    git-svn-id: https://plugins.svn.wordpress.org/syntaxhighlighter-evolved-php5/tags/1.4.1@383172 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/syntaxhighlighter-php5.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: SyntaxHighlighter Evolved: PHP5
+Description: Adds support for the PHP5 language to the SyntaxHighlighter Evolved plugin.
+Author: Sandro Bilbeisi
+Version: 1.4.1
+Author URI: http://www.sandrobilbeisi.org/
+based on the new PHP brush by David Chambers : http://davidchambersdesign.com/php-brush-for-syntaxhighlighter/
+*/
+
+// SyntaxHighlighter Evolved doesn't do anything until early in the "init" hook, so best to wait until after that
+add_action( 'init', 'syntaxhighlighter_php5_regscript' );
+
+// Tell SyntaxHighlighter Evolved about this new language/brush
+add_filter( 'syntaxhighlighter_brushes', 'syntaxhighlighter_php5_addlang' );
+
+// Register the brush file with WordPress
+function syntaxhighlighter_php5_regscript() {
+	wp_register_script( 'syntaxhighlighter-brush-php5', plugins_url('syntaxhighlighter-evolved-php5/scripts/shBrushPhp5.js'), array('syntaxhighlighter-core'), '1.4.1');
+	/*wp_register_style(  'syntaxhighlighter-theme-php5', plugins_url('syntaxhighlighter-evolved-php5/styles/shThemePHP5.css'), array('syntaxhighlighter-core'), '1.4.0');*/
+}
+
+// Filter SyntaxHighlighter Evolved's language array
+function syntaxhighlighter_php5_addlang( $brushes ) {
+	$brushes['php5'] = 'php5';
+	$brushes['php5'] = 'php5';
+	return $brushes;
+}
+?>
